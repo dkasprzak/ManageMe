c5c5405b2325b2c45c1bbe03294cf4a1cdc7847d
dkasprzak <dominik.kasprzak1408@gmail.com>
2023-07-04 15:59:25 +0200

    Added models, constatnts and components

--- manage-me/src/app/app.module.ts
+++ manage-me/src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import { ProjectsListComponent } from './module/home/projects-list/projects-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProjectsListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
